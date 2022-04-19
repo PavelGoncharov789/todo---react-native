4c0108f0db57b777a685129b3f7928806b3bb050
Pavel Goncharov <pavel.Agonchar@yandex.ru>
2022-04-19 10:41:47 +0300

    added redax

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
-import { View, StyleSheet, TextInput, Button, Alert} from 'react-native'
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button, Alert } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
 
-export const AddTodo = ({ onSubmit }) => {
-    const [value, setValue] = useState('');
+export const AddTodo = () => {
+  const [value, setValue] = useState("");
 
-    const pressHandler = () => {
-        if (value.trim()) {
-            onSubmit(value)
-            setValue('')
-        } else {
-            Alert.alert(' Нельзя добавить пустую строку ')
-        }
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state);
 
+  const addOneTodo = () => {
+    if (value.trim()) {
+      dispatch({ type: "addTodo", payload: value });
+      setValue("");
+    } else {
+      Alert.alert(" Нельзя добавить пустую строку ");
     }
+  };
 
-    return (
-        <View style={styles.wrapper}>
-            <TextInput style={styles.input}
-                onChangeText={text => setValue(text)}
-                value={value}
-                placeholder="Введите название..." 
-                autoCorrect={false}
-                autoCapitalize="none"/>
-            <Button title='Добавить' onPress={pressHandler} />
-        </View>
-    )
-}
+  return (
+    <View style={styles.wrapper}>
+      <TextInput
+        style={styles.input}
+        onChangeText={(text) => setValue(text)}
+        value={value}
+        placeholder="Введите название..."
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Button title="Добавить" onPress={addOneTodo} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    wrapper: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        marginTop: 15
-    },
-    input: {
-        width: '70%',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        padding: 2,
-        paddingLeft: 2,
-        paddingRight: 2
-    }
-})
+  wrapper: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 15,
+  },
+  input: {
+    width: "70%",
+    borderStyle: "solid",
+    borderWidth: 2,
+    padding: 2,
+    paddingLeft: 2,
+    paddingRight: 2,
+  },
+});
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 export const Navbar = (props) => {
-    const { title } = props;
-    return (
-        <View style={styles.navbar}>
-            <Text style={styles.text}> {title} </Text>
-        </View>
-    )
-}
+  const { title } = props;
+  return (
+    <View style={styles.navbar}>
+      <Text style={styles.text}> {title} </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    navbar: {
-        height: 55,
-        alignItems: 'center',
-        justifyContent: 'flex-end',
-        backgroundColor: '#00cc00',
-        paddingBottom: 10
-    },
-    text: {
-        color: "white",
-        fontSize: 18
-    }
+  navbar: {
+    height: 55,
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "#00cc00",
+    paddingBottom: 10,
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+  },
 });
